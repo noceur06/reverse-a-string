41f134cc2cbb5c98811af4822a0eb77f103c77a1
noceur06 <ya0558494@gnail.com>
2026-01-06 18:58:50 +0530

    add reverse string project

---
+++ script.js
@@ -0,0 +1,20 @@
+function reverseAstring(string){
+    for(let i = string.length - 1; i >= 0; i--){
+        console.log(string[i]);
+    }
+}
+
+console.log(reverseAstring("ezzbyte"));
+
+// in a single word 
+
+// function reverseAstring(string){
+
+//     let reveresed = "";
+//     for(let i = string.length - 1; i >= 0; i--){
+//        reveresed += string[i];
+//     }
+//     return reveresed
+// }
+
+// console.log(reverseAstring("ezzbyte"));
